468f9a17dc257376db965b1bd98ecd0c20a346b2
CinthiaRod <Cinthiarb28@gmail.com>
2024-12-13 23:03:43 -0800

    Descripción

---
+++ ejercicio13.js
@@ -0,0 +1,15 @@
+//Encuentra la película más corta y analiza los títulos largos Supón que tienes una lista de películas con su duración en minutos:
+//const movies = [
+//{ title: 'El Señor de los Anillos', duration: 200 },
+//{ title: 'Inception', duration: 148 },
+//{ title: 'Matrix', duration: 136 },
+//{ title: 'Toy Story', duration: 81 },
+//{ title: 'Coco', duration: 105 }
+//];
+//Queremos:
+//Encontrar la película más corta en duración.
+//De las películas restantes, quedarnos con aquellas cuyo título tenga más de 10 caracteres.
+//Crear una lista de los títulos largos en minúsculas.
+//Imprime en la consola:
+//El título de la película más corta.
+//La lista de títulos largos en minúsculas
