69f3c7365c29db3fd0799a9ceb20b8941c72b0c9
CinthiaRod <Cinthiarb28@gmail.com>
2024-12-13 23:01:19 -0800

    Descripción

---
+++ ejercicio8.js
@@ -0,0 +1,2 @@
+//Ejercicio 8: Busca un nombre específico
+//Tienes una lista de nombres ['Carlos', 'Daniel', 'Laura', 'Ana']. Queremos saber si el nombre 'Laura' está presente en la lista y, si lo está, deberías devolverlo.
